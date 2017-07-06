a199f58faf9b3ee4497b30ffd6edfc677ffd259e
AbhinavBehal <abhinav.behal@hotmail.com>
2017-07-06 14:10:21 +1200

    initial commit

---
+++ RemindMe/RemindMe/Models/Reminder.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace RemindMe.Models
+{
+    public class Reminder : INotifyPropertyChanged
+    {
+        private string _title;
+        private string _description;
+        private DateTime _time;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public Reminder(string title, string description, DateTime time)
+        {
+            _title = title;
+            _description = description;
+            _time = time;
+        }
+
+        public string Title
+        {
+            get => _title;
+            set
+            {
+                _title = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Description
+        {
+            get => _description;
+            set
+            {
+                _description = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public DateTime Time
+        {
+            get => _time;
+            set
+            {
+                _time = value;
+                OnPropertyChanged();
+            }
+        }
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        
+    }
+}
---
+++ RemindMe/RemindMe/ViewModels/ReminderViewModel.cs
@@ -0,0 +1,28 @@
+﻿using RemindMe.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace RemindMe.ViewModels
+{
+    public class ReminderViewModel
+    {
+        private ObservableCollection<Reminder> _remindersList;
+        private Reminder _selectedReminder;
+
+        public ReminderViewModel()
+        {
+            _remindersList = new ObservableCollection<Reminder>();
+        }
+
+        public Reminder SelectedReminder
+        {
+            get => _selectedReminder;
+            set
+            {
+                _selectedReminder = value;
+            }
+        }
+    }
+}
